af243c866b1086004532e5a9b8a3ea3a86f07997
javimv36 <javimv36@gmail.com>
2017-09-28 10:47:22 +0200

    Practica 0 y ejercicios

---
+++ practicas/p0/programa1.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    int edad;
+    printf("Introduce un numero:");
+    scanf("%d",&edad);
+    printf("%d", edad);
+    exit(0);
+}
---
+++ practicas/p0/script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "como e te llamas: "
+read nombre
+echo "Bienbenidos a SO, $nombre"
+
