d9093e2e820c2320b1615cfaf01c0953a84c2e85
javimv36 <javimv36@gmail.com>
2017-09-29 14:31:59 +0200

    Practica 0, parte 2 terminada

---
+++ practicas/p0/lib.h
@@ -0,0 +1,6 @@
+/*
+Esta es la cabecera lib.h que declara las funciones primero y segundo
+*/
+
+void segundo(char *);
+void primero(int);
---
+++ practicas/p0/programa2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib.h"
+int main(){
+    segundo("estudiantes");
+    exit(0);
+}
