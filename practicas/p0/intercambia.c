f02f70b738b9244e6ac889f160d50f10a28a2468
javimv36 <javimv36@gmail.com>
2017-10-01 14:44:43 +0200

    3.Manejo de punteros (intercambia)

---
+++ practicas/p0/intercambia.c
@@ -0,0 +1,6 @@
+void intercambia (int *a, int *b){
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
---
+++ practicas/p0/programa3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib.h"
+int main(){
+    int a = 5;
+    int b = 0;
+    printf("Antes del cambio %d\n", a);
+    intercambia (&a, &b);
+    printf("Despues del cambio %d\n", a);
+    exit(0);
+}
